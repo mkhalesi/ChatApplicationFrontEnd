1a1c1a918dc00f9308d7cb337496cd6b745da638
Mohammad Khalesi <mr.mohammadkhalesi@gmail.com>
2022-05-28 00:34:53 +0430

    refactoring PathTools.ts

--- src/app/utilities/PathTools.ts
+++ src/app/utilities/PathTools.ts
@@ -1,4 +1,8 @@
-export const ApiDomainAddress = 'https://localhost:7240';
+import {environment} from "../../environments/environment";
+
+export const ApiDomainAddress = environment.production ?
+  'http://mkhalesi-001-site1.ctempurl.com' :
+  'https://localhost:7240';
 export const ChatMethodName = 'ReceiveMessage';
 export const invokeSendMessageName = 'SendMessage';
 export const ChatAppCookieName = "ChatAppCookie";
